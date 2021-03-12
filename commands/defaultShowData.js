588169a5d8cab68d48e1a8f75a5d7f2bd1315542
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-03-12 15:55:54 +0530

    a

--- commands/defaultShowData.js
+++ commands/defaultShowData.js
@@ -4,8 +4,6 @@ module.exports = function(input){
     for (var i = 0; i < input.length; i++) {
         let dirpath = input[i];
         // console.log(dirpath);
-        if (fs.statSync(dirpath).isFile())
             showContent(input[i])
-        else console.log('Invalid File Path...!');
     }
 }
--- commands/showContent.js
+++ commands/showContent.js
@@ -1,6 +1,7 @@
 let fs = require('fs')
 module.exports = function (dirpath) {
     fs.readFile(dirpath, 'utf8', function (err, data) {
-        err ? console.log('Enter a valid file path.') : console.log(data);
+        if(err)  console.log('Enter a valid file path.') 
+        else console.log(data);
     });
 }
